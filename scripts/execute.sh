3f9a8457754c7aacfc742efc4deea7c43c680a71
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-07-28 10:53:22 -0700

    Improve MOTD. Add logging to rtorrent

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml -i production/inventory.yml -e@vars/vaulted_vars.yml --vault-password-file vault_password_file "$@"
 
